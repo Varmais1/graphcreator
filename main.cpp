f716d4e39510a86bfef9d86345d51c14fe501797
Varmais1 <418487@bsd48.org>
2023-06-08 14:29:18 -0700

    Print and add working

--- main.cpp
+++ main.cpp
@@ -11,11 +11,12 @@ void swap(vertex* all[], vertex* first, vertex* second);
 void swap(edge* all[], edge* first, edge* second);
 
 int main() {
+  cout << "done." << endl;
   vector<vertex*> graph;
   int size = 0;
   char command[15];
   while(true) {
-    cout << "Type add, delete, shortest, or quit.";
+    cout << "Type add, delete, shortest, or quit." << endl;
     cin >> command;
     singlelowercase(command);
     if(strcmp(command, "quit") == 0) {
@@ -32,7 +33,7 @@ int main() {
 	  char label;
 	  cout << "What letter would you like to be the label?" << endl;
 	  cin >> label;
-	  graph.push(new vertex(label));
+	  graph.push_back(new vertex(label));
 	  size++;
 	}
       }
@@ -53,11 +54,14 @@ int main() {
 	      break;
 	    }
 	    for(auto i = graph.begin(); i !=graph.end(); i++) {
-	      if(*i->id == label[0]) {
+	      if((*i)->id == label[0]) {
 		first = *i;
 		valid = true;
 	      }
 	    }
+	  }
+	  if(!valid) {
+	    continue;
 	  }
 	  valid = false;
 	  while(!valid) {
@@ -67,7 +71,7 @@ int main() {
 	      break;
 	    }
 	    for(auto i = graph.begin(); i != graph.end(); i++) {
-	      if(*i->id == label[0]) {
+	      if((*i)->id == label[0]) {
 		second = *i;
 		valid = true;
 	      }
@@ -84,7 +88,98 @@ int main() {
       }
     }
     else if(strcmp(command, "delete") == 0) {
-
+      cout << "Would you like to delete an edge, or an entire vertex?" << endl;
+      char type[10];
+      if(strcmp(type, "vertex") == 0) {
+	char label;
+	cout << "Which vertex would you like to delete?" << endl;
+	bool there = false;
+	cin >> label;
+	for(auto i = graph.begin(); i != graph.end(); i++) {
+	  if((*i)->id == label) {
+	    graph.erase(i);
+	    there = true;
+	    break;
+	  }
+	}
+	if(there) {
+	  continue;
+	}
+	else {
+	  cout << "That vertex does not exists." << endl;
+	  continue;
+	}
+      }
+      else if(strcmp(type, "edge") == 0) {
+	cout << "Which vertices does the edge connect?" << endl;
+	vertex* first;
+	vertex* second;
+	char label1;
+	char label2;
+	cin >> label1;
+	cin >> label2;
+	bool valid1 = false;
+	bool valid2 = false;
+	for(auto i = graph.begin(); i != graph.end(); i++) {
+	  if((*i)->id == label1) {
+	    first = *i;
+	    valid1 = true;
+	  }
+	  else if((*i)->id == label2) {
+	    second = *i;
+	    valid2 = true;
+	  }
+	}
+	if(valid1 && valid2) {
+	  bool exists = false;
+	  edge* temp;
+	  for(int i = 0; i < first->size; i++) {
+	    if(first->all[i]->otherPoint(first) == second) {
+	      temp = first->all[i];
+	      exists = true;
+	      if(i != first->size - 1) {
+		swap(first->all, first->all[i], first->all[first->size-1]);
+	      }
+	      first->all[size-1] = NULL;
+	      first->size--;
+	    }
+	  }
+	  if(exists) {
+	    for(int i = 0; i< second->size; i++) {
+	      if(second->all[i]->otherPoint(second) == first) {
+		if(i != second->size - 1) {
+		  swap(second->all, second->all[i], second->all[second->size - 1]);
+		}
+		second->all[size-1] = NULL;
+		second->size--;
+		delete temp;
+	      }
+	    }
+	  }
+	  else {
+	    cout << "These vertices don't have an edge between them." << endl;
+	  }
+	}
+	else {
+	  cout << "Please enter 2 valid vertices." << endl;
+	}
+      }
+      else {
+	cout << "Please enter a valid input of edge or vertex." << endl; 
+      }
+    }
+    else if(strcmp(command, "print") == 0) {
+      if(graph.empty()) {
+	cout << "There are no vertices, please add at least one." << endl;
+	continue;
+      }
+      for(auto i = graph.begin(); i != graph.end(); i++) {
+	cout << "Vertex: " << (*i)->id << endl;
+	for(int j = 0; j < (*i)->size; j++) {
+	  cout << (*i)->all[j]->otherPoint(*i)->id << ": " << (*i)->all[j]->weight << "\t"; 
+	}
+	cout << endl;
+      }
     }
   }
   return 0;
--- vertex.cpp
+++ vertex.cpp
@@ -1,5 +1,8 @@
 #include "vertex.h"
 
+void swap1(edge* all[], edge* first, edge* second);
+void swap1(vertex* all[], vertex* first, vertex* second);
+
 vertex::vertex(char nid) {
   id = nid;
 }
@@ -7,7 +10,50 @@ vertex::vertex(char nid) {
 vertex::~vertex() {
   for(int i = 0; i < size; i++) {
     if(all[i] != NULL) {
+      vertex* other = all[i]->otherPoint(this);
+      for(int j = 0; j < other->size; j++) {
+	if(other->all[j] = all[i]) {
+	  if(j != other->size - 1) {
+	    swap1(other->all, other->all[j], other->all[other->size-1]);
+	  }
+	  other->all[size - 1] = NULL;
+	  other->size--;
+	}
+      }
       delete all[i];
     }
   }
+}
+
+
+
+void swap1(edge* all[], edge* first, edge* second) {
+  int firstPos;
+  int secondPos;
+  for(int i = 0; i < 20; i++) {
+    if(all[i] == first) {
+      firstPos = i;
+    }
+    else if(all[i] == second) {
+      secondPos = i;
+    }
+  }
+  all[firstPos] = second;
+  all[secondPos] = first;
+}
+
+void swap1(vertex* all[], vertex* first, vertex* second) {
+  int firstPos;
+  int secondPos;
+  for(int i = 0; i < 20; i++) {
+    if(all[i] == first) {
+      firstPos = i;
+    }
+    else if(all[i] == second) {
+      secondPos = i;
+    }
+  }
+  all[firstPos] = second;
+  all[secondPos] = first;
+
 }
